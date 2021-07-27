84a0c46ab54329b05526ea4b653268065a8d7b30
tomasArtaza77 <tomasartaza25@gmail.com>
2021-07-27 15:00:58 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje = '' ;
+  registrado = false;
+  nombrE:string = "";
+  apellidO:string = "";
+  entradas: object[]; 
+
+  constructor(){
+    this.entradas = [
+      {titulo: "python"},
+      {titulo: "java"},
+      {titulo: "angular"},
+      {titulo: "css"},
+      {titulo: "JS"},
+    ];
+  }
+
+
+  registrarUsuario(){
+    this.mensaje = "Usuario registrado satisfactoriamente." 
+    this.registrado = true;
+  }
+
+
 }
+
